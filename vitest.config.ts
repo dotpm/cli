efc3a60a50492c7f2145e4ba465bdbfafe403ca7
Jason Hyde <github@2bad.me>
2026-09-15 21:56:37 +0200

    move the command line out of the plugin repository

---
+++ vitest.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  define: { __CLI_VERSION__: JSON.stringify('0.0.0-test') },
+  test: { include: ['src/**/*.test.ts'] }
+})
